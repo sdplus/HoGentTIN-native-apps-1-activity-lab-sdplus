59b673415c13021739ab60baf74b80053b9eb896
sdplus <deplussofie@gmail.com>
2016-10-08 18:39:44 +0200

    Update Activity2.java

--- app/src/main/java/hogent/activitylab/Activity2.java
+++ app/src/main/java/hogent/activitylab/Activity2.java
@@ -1,6 +1,7 @@
 package hogent.activitylab;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,13 +21,23 @@ public class Activity2 extends Activity {
      * TODO: Creëer counter variabelen voor onCreate(), onRestart(), onStart() and
      * onResume() etc.
      * Je moet deze variabelen incremeteren elke keer de overeenkomstige methode opgeroepen wordt
-
      */
+    private int mCreate = 0;
+    private int mRestart = 0;
+    private int mStart = 0;
+    private int mResume = 0;
+    private int mPause = 0;
+    private int mStop = 0;
+    private int mDestroy = 0;
 
 
     /**
      * TODO: Creëer variabelen voor de verschillende textviews
      */
+    private TextView mCreateView;
+    private TextView mStartView;
+    private TextView mResumeView;
+    private TextView mRestartView;
 
 
     // TODO: Creëer variabelen voor de verschillende textviews
@@ -39,27 +50,43 @@ public class Activity2 extends Activity {
         // TODO: initialiseer de correcte TextViews
         // Hint: findViewById()
         // textView1 = (TextView) findViewById(R.id.textView1);
+        mCreateView = (TextView) findViewById(R.id.create);
+        mStartView = (TextView) findViewById(R.id.start);
+        mResumeView = (TextView) findViewById(R.id.resume);
+        mRestartView = (TextView) findViewById(R.id.restart);
 
 
         //TODO: voeg een Onclicklistener toe aan de button om de activity te stoppen
-
+        Button btnClose = (Button) findViewById(R.id.bClose);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 
 
         // TODO: Check voor gesavede state
-        if () {
+        if (savedInstanceState != null) {
 
             // TODO: Herstel waarde van de counters van de  saved state
+            mCreate = savedInstanceState.getInt("mCreate");
+            mStart = savedInstanceState.getInt("mStart");
+            mResume = savedInstanceState.getInt("mResume");
+            mRestart = savedInstanceState.getInt("mRestart");
 
         }
 
         // TODO: LogCat message
-
+        Log.i(TAG, "Create has been called");
 
         // TODO:Update voor de variabelen
+        mCreate++;
 
 
         // TODO: Update de user interface via de displayCounts() methode
+        displayCounts();
 
 
     }
@@ -86,10 +113,13 @@ public class Activity2 extends Activity {
         super.onResume();
 
         // TODO: LogCat message
+        Log.i(TAG, "Resume has been called");
 
         // TODO: Update de  count variable
+        mResume++;
 
         // TODO: Update the user interfac
+        displayCounts();
 
 
     }
@@ -99,10 +129,13 @@ public class Activity2 extends Activity {
         super.onPause();
 
         // TODO: LogCat message
+        Log.i(TAG, "Pause has been called");
 
         // TODO: Update de  count variable
+        mPause++;
 
         // TODO: Update the user interfac
+        displayCounts();
 
 
     }
@@ -112,10 +145,13 @@ public class Activity2 extends Activity {
         super.onStop();
 
         // TODO: LogCat message
+        Log.i(TAG, "Stop has been called");
 
         // TODO: Update de  count variable
+        mStop++;
 
         // TODO: Update the user interfac
+        displayCounts();
 
 
     }
@@ -125,10 +161,13 @@ public class Activity2 extends Activity {
         super.onRestart();
 
         // TODO: LogCat message
+        Log.i(TAG, "Restart has been called");
 
         // TODO: Update de  count variable
+        mRestart++;
 
         // TODO: Update the user interfac
+        displayCounts();
 
     }
 
@@ -137,10 +176,13 @@ public class Activity2 extends Activity {
         super.onDestroy();
 
         // TODO: LogCat message
+        Log.i(TAG, "Destroy has been called");
 
         // TODO: Update de  count variable
+        mDestroy++;
 
         // TODO: Update the user interfac
+        displayCounts();
     }
 
     @Override
@@ -148,7 +190,10 @@ public class Activity2 extends Activity {
 
         // TODO:  Save de state  met key-value pairs in de Bundle
 
-
+        savedInstanceState.putInt("mCreate", mCreate);
+        savedInstanceState.putInt("mStart", mStart);
+        savedInstanceState.putInt("mResume", mResume);
+        savedInstanceState.putInt("mRestart", mRestart);
 
 
 
@@ -158,6 +203,10 @@ public class Activity2 extends Activity {
     public void displayCounts() {
 
         // TODO: zet de countervariabelen correct in de activity
+        mCreateView.setText(getResources().getString(R.string.onCreate) + " " + mCreate);
+        mStartView.setText(getResources().getString(R.string.onStart) + " " + mStart);
+        mResumeView.setText(getResources().getString(R.string.onResume) + " " + mResume);
+        mRestartView.setText(getResources().getString(R.string.onRestart) + " " + mRestart);
 
     }
 }
